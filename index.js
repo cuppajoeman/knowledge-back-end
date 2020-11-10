56c622abc95ebb3875728f435839252f19862fe6
ccn <callum.cassidynolan@mail.utoronto.ca>
2020-11-10 21:53:24 +0000

    add in deps

--- index.js
+++ index.js
@@ -1,18 +1,17 @@
-//Load HTTP module
-const http = require("http");
-const hostname = '127.0.0.1';
-const port = 3000;
+import { typeDefs } from './graphql-schema.js'
 
-//Create HTTP server and listen on port 3000 for requests
-const server = http.createServer((req, res) => {
+const { ApolloServer } = require("apollo-server");
+const neo4j = require("neo4j-driver");
+const { makeAugmentedSchema } = require("neo4j-graphql-js");
 
-  //Set the response HTTP header with HTTP status and Content type
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
+const schema = makeAugmentedSchema({
+  typeDefs
 });
 
-//listen for request on port 3000, and as a callback function have the port listened on logged
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+const server = new ApolloServer({
+  schema
+});
+
+server.listen().then(({ url }) => {
+  console.log(`GraphQL server ready at ${url}`);
 });
